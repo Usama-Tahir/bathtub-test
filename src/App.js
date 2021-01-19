e93325c8f3a7d10eda8fb6be195226a81ffcdd92
Usama Tahir <14beseutahir@seecs.edu.pk>
2021-01-19 06:26:29 +0500

    Water flow management.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // keep track of water level
+  const [waterLevel, setWaterlevel] = useState(0);
+  // check if water level is increasing or decreasing.
+  const [waterFlow, setWaterFlow] = useState(null);
+  // keep track of current interval id.
+  const [clearintervalId, setClearIntervalId] = useState(null);
+
+  useEffect(() => {
+    // we check if water bath tub is filled or empty and clear interval.
+    const condition = (waterFlow && waterFlow === 'up' && waterLevel === 5) || (waterFlow && waterFlow === 'down' && waterLevel === 0);
+    if (condition) {
+      clearInterval(clearintervalId);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [waterLevel, waterFlow]);
+
+  const increaseWaterLevel = () => {
+    setWaterFlow('up');
+    clearInterval(clearintervalId);
+    // don't set up time interval if water level is already 5.
+    if (waterLevel === 5) {
+      return;
+    }
+    const intervalId = setInterval(() => {
+      setWaterlevel(prevWater => prevWater + 1);
+    }, 2000);
+    setClearIntervalId(intervalId);
+  };
+
+  const decreaseWaterLevel = () => {
+    clearInterval(clearintervalId);
+    setWaterFlow('down');
+    // don't set down time interval if water level is already 0.
+    if (waterLevel === 0) {
+      return;
+    }
+    const intervalId = setInterval(() => {
+      setWaterlevel(prevWater => prevWater - 1);
+    }, 2000);
+    setClearIntervalId(intervalId);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <div className="btn-container">
+        <button className="button" onClick={increaseWaterLevel}>+</button>
+        <button className="button" onClick={decreaseWaterLevel}>-</button>
+      </div>
+      {waterLevel === 5 && <div className="danger">Oh No! Water overflow</div>}
+      <div className='water-level-display-container'>
+        {[...Array(waterLevel)].map((e, i) => <div className="water-level-display-children">{waterLevel - i}</div>)
+}
+      </div>
     </div>
   );
 }
